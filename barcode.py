208e44818bead05f69780dbcfdb9bae52b269104
anjiwon319 <anjiwon319@kookmin.ac.kr>
2020-11-30 14:52:49 +0900

    module barcode: remove useless code

--- barcode.py
+++ barcode.py
@@ -15,16 +15,8 @@ class Barcode:
             decoded = pyzbar.decode(gray)
 
             for d in decoded:
-                x, y, w, h = d.rect
-
                 barcode_data = d.data.decode("utf-8")
-                barcode_type = d.type
                 return barcode_data
-
-                cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
-
-                text = '%s (%s)' % (barcode_data, barcode_type)
-                cv2.putText(img, text, (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255), 2, cv2.LINE_AA)
 
             cv2.imshow('img', img)
 
